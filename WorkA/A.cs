9673be4bf08981b6dde5d9eb614b46692771c4e7
Charlie Brown <cbrown@brock.ac.uk>
2023-05-04 12:25:29 +0100

    Added A

---
+++ WorkA/A.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkA
+{
+    internal class A
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first word");
+            string first = Console.ReadLine();
+            Console.WriteLine("Enter the second word");
+            string second = Console.ReadLine(); 
+            bool isOk = false;
+            // loop through first
+            for (int i = 0; i < first.Length; i++)
+            {
+                string letterFromFirst = first.Substring(i, 1);
+                isOk = false;
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if (letterFromFirst == second.Substring(j,1))
+                    {
+                        second = second.Remove(j, 1);
+                        isOk = true;
+                        break;
+                    }
+                }
+                if (!isOk)
+                {
+                    break;
+                }
+            }
+            if (isOk)
+            {
+                Console.WriteLine("good");
+            }
+            else
+            {
+                Console.WriteLine("bad");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
