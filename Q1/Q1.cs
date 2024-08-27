7609c8266c5599876f5bf96ef1c4a2c349e85959
Charlie Brown <CBROWN@brock.ac.uk>
2024-08-27 15:02:24 +0100

    Q1 and Q2

---
+++ Q1/Q1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Q1
+    {
+        static void Main(string[] args)
+        {
+            string compressedText = "";
+            int countOfLetter = 1;
+            string currentLetter = "";
+            Console.WriteLine("Enter the text to compress");
+            string originalText = Console.ReadLine();
+            int pos = 0;
+            currentLetter = originalText[0].ToString();
+            while (pos < originalText.Length -1)
+            {
+                if (currentLetter != originalText[pos +1].ToString())
+                {
+                    compressedText = compressedText + currentLetter + countOfLetter;
+                    currentLetter = originalText[pos + 1].ToString();
+                    countOfLetter = 1;
+                    pos ++;
+                }
+                else
+                {
+                    pos++;
+                    countOfLetter++;
+                }
+            }
+            compressedText = compressedText + currentLetter + countOfLetter;
+            Console.WriteLine(compressedText);
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a decimal number");
+            int decNum = Convert.ToInt32(Console.ReadLine());
+            string decStr = "";
+            do
+            {
+                decStr = (decNum % 2) + decStr;
+                decNum = decNum / 2;
+            } while (decNum != 0);
+            Console.WriteLine(decStr);
+            Console.ReadLine();
+        }
+    }
+}
