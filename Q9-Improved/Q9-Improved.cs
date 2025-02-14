8f7ec09b471addbf65f2a36a8ecb5656cd32102a
Charlie Brown <CBROWN@brock.ac.uk>
2025-02-14 11:28:26 +0000

    Q9Improved

---
+++ Q9-Improved/Q9-Improved.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q9
+{
+    internal class Q9
+    {
+        static void Main(string[] args)
+        {
+            // bouncy numbers
+            int num = 0;
+            do
+            {
+                Console.WriteLine("enter a number");
+                num = Convert.ToInt32(Console.ReadLine());
+
+            } while (num <= 0);
+            string strNum = num.ToString();
+            int countOfIncreasing = 0;
+            int countOfDecreasing = 0;
+          
+            for (int i = 0; i < strNum.Length - 1; i++)
+            {
+                if (strNum[i] < strNum[i + 1])
+                {
+                    countOfIncreasing++;
+                }
+                else if (strNum[i] > strNum[i + 1])
+                {
+                    countOfDecreasing++;
+                }
+            }
+            if (countOfDecreasing != 0 && countOfIncreasing == 0)
+            {
+                Console.WriteLine("Not bouncy");
+            }
+            else if (countOfIncreasing != 0 && countOfDecreasing == 0)
+            {
+                Console.WriteLine("not a bouncy number");
+            }
+            else if (countOfIncreasing !=0 && countOfDecreasing != 0 && countOfIncreasing != countOfDecreasing)
+            {
+                Console.WriteLine("bouncy");
+            }
+            else
+            {
+                Console.WriteLine("perfectly bouncy");
+            }
+           
+            //if (!increasing && !decreasing)
+            //{
+            //    if (countOfIncreasing == countOfDecreasing)
+            //    {
+            //        Console.WriteLine("perfectly bouncy");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("bouncy");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not bouncy");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
