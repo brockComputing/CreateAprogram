7608ad4538379c885e263d5b74845af4e9ab31f8
Charlie Brown <cbrown@brock.ac.uk>
2025-04-30 10:13:55 +0100

    tried a better version on Q7 gave up

--- Q7/Q7.cs
+++ Q7/Q7.cs
@@ -31,7 +31,7 @@ namespace Q7
                             enteredText = enteredText.Remove(startpos, 1);
                             enteredText = enteredText.Insert(startpos, second);
                             enteredText = enteredText.Remove(endpos, 1);
-                            enteredText = enteredText.Insert(endpos,first);
+                            enteredText = enteredText.Insert(endpos, first);
                             // exit loop
                             endpos--;
                             break;
@@ -42,10 +42,39 @@ namespace Q7
                 startpos++;
             }
             Console.WriteLine(enteredText);
+
+            // possibly a better method is to record index of vowels in a list and the actual vowels found
+            // in another list reverse one of the lists and then swap
+            // this does not work
+
+            //string vowels = "aeiou";
+            //string enteredText = "";
+            //Console.WriteLine("enter a word");
+            //enteredText = Console.ReadLine();
+            //List<int> indexes = new List<int>();
+            //List<string> vowelsUsed = new List<string>();
+            //// find the vowels and their indexes
+            //int pos = 0;
+            //foreach (var item in enteredText)
+            //{
+            //    if (vowels.Contains(item))
+            //    {
+            //        indexes.Add(pos);
+            //        vowelsUsed.Add(item.ToString());
+            //    }
+            //    pos++;
+            //}
+            //List<int> reversedIndexed = indexes.ToList(); // a complete new copt
+            //reversedIndexed.Reverse();
+            //for (int i = 0; i < indexes.Count; i++)
+            //{
+            //    string currentChar = vowelsUsed[i];
+            //    enteredText = enteredText.Remove(indexes[i], 1);
+            //    enteredText = enteredText.Insert(reversedIndexed[i], currentChar);
+            //}
+            //Console.WriteLine(enteredText);
             Console.ReadLine();
         }
-        // possibly a better method is to record index of vowels in a list and the actual vowels found
-        // in another list reverse one of the lists and then swap
-       
+
     }
 }
